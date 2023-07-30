ad676d71b3523044c528625962c2296463e7ec57
amo <115477511+muamelAmeer@users.noreply.github.com>
2023-07-30 22:44:01 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,22 +1,9 @@
 import requests
 from t import list as l
-def check_proxy(proxy):
+def check_proxy(proxy,user):
     try:
-        response = requests.get("https://t.me/vv111", proxies={"http": proxy, "https": proxy})
-        if response.status_code == 200:
-            return True
-        else:
-            return False
-    except requests.exceptions.RequestException:
-        return False
+        response = requests.get(f"https://t.me/{user}", proxies={"http": proxy, "https": proxy})
+        if response.status_code == 200:return True
+        else:return False
     except:return False
   
-input("inter FOr RUN")
-  
-for proxy in l:
-    if check_proxy(proxy) == True:
-        with open("Good.txt", "a") as file:
-            file.write(proxy + "\n")
-        print(f"Proxy {proxy} True \n")
-    else:
-        print(f"Proxy {proxy} False \n")
